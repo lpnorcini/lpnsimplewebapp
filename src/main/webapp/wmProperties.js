7a57a569f38ea646d020dbae44f4071d0175d301
root <root@54208e76e50b>
2017-04-13 19:14:56 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "lpnsimplewebapp",
+  "homePage" : "Main",
+  "name" : "lpnsimplewebapp",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
